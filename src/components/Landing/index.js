a68773e6e7acfe046813dda423d5e47a431b62b8
rivofaustav <rivofaustav.rv@gmail.com>
2020-05-13 20:12:04 +0700

    adding bootstrap react

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Landing = () => (
-  <div>
+  <Container fluid>
+    <Row>
+      <Col>
     <h1>Welcome to Message App</h1>
-    <h3>by Rivo Valiano</h3>
-  </div>
+    <h5>by Rivo Valiano</h5>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Landing;
